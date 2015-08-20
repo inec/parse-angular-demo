a648cfc2faaace72e2ac9667b3f29d7ac5e6ff12
Y-Chi Lu <luyege+github@gmail.com>
2015-08-19 23:52:58 -0500

    test

--- client/app/scripts/services/ParseServices.js
+++ client/app/scripts/services/ParseServices.js
@@ -2,7 +2,8 @@ angular.module('ParseServices', [])
 .factory('ParseSDK', function() {
 
   // pro-tip: swap these keys out for PROD keys automatically on deploy using grunt-replace
-  Parse.initialize("oInJV2JlcXR96EN0SQjee0n5FlRUAd0joyFXReve", "OOZa6rsbkIiRWyiNlmHVMgMIHAyTqPdHRg7N1PxO");
+  Parse.initialize("OOq7zpzbzpEPjM7nWVJpbD7AKn8atU6f49CBweoF", "PXUcZ84TBsMLhxHFhqB2EuBZzLxiKZJmcwlnKeNh");
+  //Parse.initialize("oInJV2JlcXR96EN0SQjee0n5FlRUAd0joyFXReve", "OOZa6rsbkIiRWyiNlmHVMgMIHAyTqPdHRg7N1PxO");
 
   // FACEBOOK init
   window.fbPromise.then(function() {
@@ -10,8 +11,8 @@ angular.module('ParseServices', [])
     Parse.FacebookUtils.init({
 
       // pro-tip: swap App ID out for PROD App ID automatically on deploy using grunt-replace
-      appId: 481650395275919, // Facebook App ID
-      channelUrl: 'http://brandid.github.io/parse-angular-demo/channel.html', // Channel File
+      appId: 386206958244456, // Facebook App ID 386206958244456 481650395275919
+      channelUrl: 'http://inec.github.io/parse-angular-demo/channel.html', // Channel File
       cookie: true, // enable cookies to allow Parse to access the session
       xfbml: true, // parse XFBML
       frictionlessRequests: true // recommended
